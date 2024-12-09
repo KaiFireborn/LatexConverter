77bd530ae71a1e39e13e9f3abb52fb830a5a4d28
KaiFireborn <blitzmasterev3@gmail.com>
2024-12-09 19:35:29 +0100

    feat: add clipboard input/output

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import json
 from mapping import mapping as mapping_read
+import pyperclip
 
 
-def prompt():
+CLIPBOARD_INPUT = True
+
+
+def prompt_for_input():
     expression = input("Enter a unicode expression:\n")
     return expression
 
+def get_input_from_clipboard():
+    expression = pyperclip.paste()
+    return expression
+    
 
 def output(parsed):
     print("The LaTeX equivalent is:")
     print(parsed)
     print("\n")
+
+def output_to_clipboard(parsed):
+    pyperclip.copy(parsed)
+    print("The unicode expression from your clipboard has been converted to LaTeX and copied back to your clipboard.")
 
 
 class Parser:
@@ -19,16 +31,22 @@ class Parser:
 
     def parse_to_latex(self, expression):
         # TODO: Rewrite continuous strings under \mathit{#} so their letters are written closer to one another
-        
+        # TODO: instead of the " ".join come up with better logic so 'false' doesn't become 'f a l s e'
+
         parsed_symbols = [self.mapping.get(symbol, symbol) for symbol in expression]
         parsed_expression = "$" + " ".join(parsed_symbols) + "$"
-        
+
         return parsed_expression
 
 
 if __name__ == "__main__":
-    while True:
-        expression = prompt()
-        p = Parser()
+    p = Parser()
+    if CLIPBOARD_INPUT:
+        expression = get_input_from_clipboard()
         parsed = p.parse_to_latex(expression)
-        output(parsed)
+        output_to_clipboard(parsed)
+    else:
+        while True:
+            expression = prompt_for_input()
+            parsed = p.parse_to_latex(expression)
+            output(parsed)
